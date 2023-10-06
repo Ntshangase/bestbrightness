4ebda3bd03a48d0d214ecb90bf303c81922808e7
Simphiwe Ntshangase <simphiwe323i@gmail.com>
2023-10-06 23:17:59 +0200

    products razor page

---
+++ bestbrightness/Pages/Products/Products.cshtml.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace bestbrightness.Pages.Products
+{
+    public class ProductsModel : PageModel
+    {
+        // List to store iterations of ProductInfo
+        public List<ProductInfo> listProducts = new List<ProductInfo>();
+
+        public void OnGet()
+        {
+            try
+            {
+                // connect to database
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM Products";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = new command.ExecutableReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ProductInfo productInfo = new ProductInfo();
+                                productInfo.id = "" + reader.GetInt32(0);
+                                productInfo.item = reader.GetString(1);
+                                productInfo.category = reader.GetString(2);
+                                productInfo.price = reader.GetString(3);
+                                productInfo.review = reader.GetString(4);
+                                productInfo.created_at = reader.GetString(5).ToString();
+                            }
+                        }
+                    }
+                }
+            }catch (Exception ex) { }
+        }
+    }
+
+    // A new class to store information about 1 product
+    public class ProductInfo
+    {
+        // variables to store data from database:
+        public String id;
+        public String item;
+        public String category;
+        public String price;
+        public String review;
+        public String created_at;
+    }
+}
